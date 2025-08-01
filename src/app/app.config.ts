d0945025eccb167a19b8090f4a0c33567755a2e8
blackfellows <6dfdac64-b799-4068-9972-4a1ea68da044@blackfellows.anonaddy.me>
2025-08-01 12:21:26 +0200

    added http client with fetch and auth interceptor

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,13 +4,18 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { authConfig } from './auth/auth.config';
 import { provideAuth } from 'angular-auth-oidc-client';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
+import { authInterceptor } from './_interceptors/auth-interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes), provideAuth(authConfig),
-    provideHttpClient()
+    provideRouter(routes),
+    provideAuth(authConfig),
+    provideHttpClient(
+      withFetch(),
+      withInterceptors([authInterceptor]) // Assuming authInterceptor is defined elsewhere
+    )
   ]
 };
