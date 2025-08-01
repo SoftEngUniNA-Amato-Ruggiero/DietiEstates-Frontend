04a1128434f4f378bf5e6a5ff68a1d8988954443
blackfellows <6dfdac64-b799-4068-9972-4a1ea68da044@blackfellows.anonaddy.me>
2025-08-01 17:30:47 +0200

    home constructor sends post request to user service

--- src/app/homepage/homepage.ts
+++ src/app/homepage/homepage.ts
@@ -1,7 +1,11 @@
-import { Component, inject } from '@angular/core';
+import { Component, effect, inject } from '@angular/core';
 import { AsyncPipe, JsonPipe } from '@angular/common';
 import { AuthService } from '../_services/auth-service';
 import { MapComponent } from "../map-component/map-component";
+import { UserServiceClient } from '../_services/clients/user-service-client';
+import { User } from '../_model/user';
+import { Credentials } from '../_model/credentials';
+import { UserInfo } from '../_model/user-info';
 
 @Component({
   selector: 'app-homepage',
@@ -11,4 +15,41 @@ import { MapComponent } from "../map-component/map-component";
 })
 export class Homepage {
   protected readonly authService = inject(AuthService);
+  private readonly userClient = inject(UserServiceClient);
+
+  constructor() {
+    effect(() => {
+      if (this.authService.isAuthenticated()) {
+        this.authService.userData$.subscribe({
+          next: (userData) => {
+            const email = userData.userData.email;
+            const cognitoSub = userData.userData.sub;
+
+            const firstName = userData.userData.given_name;
+            const lastName = userData.userData.family_name;
+
+            const groups = userData.userData['cognito:groups'] || [];
+
+            const credentials = new Credentials(email, cognitoSub);
+            const info = new UserInfo(firstName, lastName);
+
+            const user = new User(credentials, info);
+
+            console.info(user, groups);
+            this.userClient.postCustomer(user).subscribe({
+              next: (response) => {
+                console.info('User created in the database:', response);
+              },
+              error: (err) => {
+                console.info('User exists in the database:', err);
+              }
+            });
+          },
+          error: (err) => {
+            console.error('Error fetching user data', err);
+          }
+        });
+      }
+    });
+  }
 }
