6b511829231b5a4021b281be9bd370896a304d41
blackfellows <6dfdac64-b799-4068-9972-4a1ea68da044@blackfellows.anonaddy.me>
2025-08-01 11:03:27 +0200

    added ngx leaflet map component

--- src/app/homepage/homepage.ts
+++ src/app/homepage/homepage.ts
@@ -1,10 +1,11 @@
 import { Component, inject } from '@angular/core';
 import { AsyncPipe, JsonPipe } from '@angular/common';
 import { AuthService } from '../_services/auth-service';
+import { MapComponent } from "../map-component/map-component";
 
 @Component({
   selector: 'app-homepage',
-  imports: [AsyncPipe, JsonPipe],
+  imports: [AsyncPipe, JsonPipe, MapComponent],
   templateUrl: './homepage.html',
   styleUrl: './homepage.scss'
 })
