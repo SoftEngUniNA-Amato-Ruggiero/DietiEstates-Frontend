4d3eff6e31fa82d5b03dc5150b7b2c8d446763dd
blackfellows <6dfdac64-b799-4068-9972-4a1ea68da044@blackfellows.anonaddy.me>
2025-08-02 08:32:29 +0200

    refactored user state logic

--- src/app/_services/auth-service.ts
+++ src/app/_services/auth-service.ts
@@ -1,40 +1,76 @@
-import { Injectable, inject, signal } from '@angular/core';
-import { OidcSecurityService } from 'angular-auth-oidc-client';
+import { Injectable, computed, effect, inject, signal } from '@angular/core';
+import { OidcSecurityService, UserDataResult } from 'angular-auth-oidc-client';
+import { User } from '../_model/user';
+import { Credentials } from '../_model/credentials';
+import { UserInfo } from '../_model/user-info';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  public readonly isAuthenticated = computed(() => this.isAuthenticatedSignal());
+  public readonly user = computed(() => this.authenticatedUser());
+
   private readonly oidcSecurityService = inject(OidcSecurityService);
+  private readonly configuration$ = this.oidcSecurityService.getConfiguration();
 
-  configuration$ = this.oidcSecurityService.getConfiguration();
+  public readonly userData$ = this.oidcSecurityService.userData$;
 
-  userData$ = this.oidcSecurityService.userData$;
-
-  isAuthenticated = signal(false);
+  private readonly isAuthenticatedSignal = signal(false);
+  private readonly authenticatedUser = signal<User | null>(null);
 
   constructor() {
     this.oidcSecurityService.checkAuth().subscribe();
 
     this.oidcSecurityService.isAuthenticated$.subscribe(
       ({ isAuthenticated }) => {
-        this.isAuthenticated.set(isAuthenticated);
+        this.isAuthenticatedSignal.set(isAuthenticated);
 
         console.warn('authenticated: ', isAuthenticated);
       }
     );
+
+    effect(() => {
+      if (!this.isAuthenticated()) {
+        return;
+      }
+      this.userData$.subscribe({
+        next: (userData) => {
+          this.initializeAuthenticatedUser(userData);
+        }
+      });
+    });
   }
 
-  login(): void {
+  public login(): void {
     this.oidcSecurityService.authorize();
   }
 
-  logout(): void {
-    // Clear session storage
+  public logout(): void {
     if (window.sessionStorage) {
       window.sessionStorage.clear();
     }
 
     window.location.href = "https://eu-west-3tkwuxva4t.auth.eu-west-3.amazoncognito.com/logout?client_id=5qdd485015k9doea0l14jcv5k0&logout_uri=http://localhost:4200";
+  }
+
+  private initializeAuthenticatedUser(userData: UserDataResult) {
+    const email: string | undefined = userData.userData.email;
+    const cognitoSub: string | undefined = userData.userData.sub;
+
+    const firstName: string | undefined = userData.userData.given_name;
+    const lastName: string | undefined = userData.userData.family_name;
+
+    const groups: string[] = userData.userData['cognito:groups'] ?? [];
+
+    if (!email || !cognitoSub || !firstName || !lastName) {
+      throw new Error('Missing user data: email, cognitoSub, firstName, or lastName is undefined. But isAuthenticated is true.');
+    }
+
+    const credentials = new Credentials(email, cognitoSub);
+    const info = new UserInfo(firstName, lastName);
+    this.authenticatedUser.set(new User(credentials, info));
+
+    console.info(this.user(), groups);
   }
 }
--- src/app/homepage/homepage.ts
+++ src/app/homepage/homepage.ts
@@ -1,11 +1,8 @@
 import { Component, effect, inject } from '@angular/core';
 import { AsyncPipe, JsonPipe } from '@angular/common';
 import { AuthService } from '../_services/auth-service';
-import { MapComponent } from "../map-component/map-component";
 import { UserServiceClient } from '../_services/clients/user-service-client';
-import { User } from '../_model/user';
-import { Credentials } from '../_model/credentials';
-import { UserInfo } from '../_model/user-info';
+import { MapComponent } from "../map-component/map-component";
 import { TestButtons } from '../test-buttons/test-buttons';
 
 @Component({
@@ -20,37 +17,18 @@ export class Homepage {
 
   constructor() {
     effect(() => {
-      if (this.authService.isAuthenticated()) {
-        this.authService.userData$.subscribe({
-          next: (userData) => {
-            const email = userData.userData.email;
-            const cognitoSub = userData.userData.sub;
-
-            const firstName = userData.userData.given_name;
-            const lastName = userData.userData.family_name;
-
-            const groups = userData.userData['cognito:groups'] || [];
-
-            const credentials = new Credentials(email, cognitoSub);
-            const info = new UserInfo(firstName, lastName);
-
-            const user = new User(credentials, info);
-
-            console.info(user, groups);
-            this.userClient.postCustomer(user).subscribe({
-              next: (response) => {
-                console.info('User created in the database:', response);
-              },
-              error: (err) => {
-                console.info('User exists in the database:', err);
-              }
-            });
-          },
-          error: (err) => {
-            console.error('Error fetching user data', err);
-          }
-        });
+      const user = this.authService.user();
+      if (!user) {
+        return;
       }
+      this.userClient.postCustomer(user).subscribe({
+        next: (response) => {
+          console.info('User created in the database:', response);
+        },
+        error: (err) => {
+          console.info('User exists in the database:', err);
+        }
+      });
     });
   }
 }
