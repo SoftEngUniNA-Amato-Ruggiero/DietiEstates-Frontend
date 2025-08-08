079b1ee1c7e369201a78943f118c13f1fd743d06
blackfellows <6dfdac64-b799-4068-9972-4a1ea68da044@blackfellows.anonaddy.me>
2025-08-08 10:50:25 +0200

    map: set markers and get user position

--- src/app/map-component/map-component.ts
+++ src/app/map-component/map-component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, computed, effect, signal } from '@angular/core';
 import { LeafletModule } from '@bluehalo/ngx-leaflet';
-import { latLng, tileLayer, icon, Icon } from 'leaflet';
+import { latLng, tileLayer, icon, Icon, LatLng, Marker, marker } from 'leaflet';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-map-component',
@@ -9,18 +10,56 @@ import { latLng, tileLayer, icon, Icon } from 'leaflet';
   styleUrl: './map-component.scss'
 })
 export class MapComponent {
+
+  public readonly clickedAt = computed(() => this.clickedAtSignal());
+
+  protected userMarkerLayer: Array<Marker> = [];
+
+  protected readonly options = {
+    layers: [
+      tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { maxZoom: 18, attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>' })
+    ],
+    zoom: 16,
+    center: latLng(40.828172, 14.190613)
+  };
+
   private readonly icon = icon({
     ...Icon.Default.prototype.options,
     iconUrl: 'assets/marker-icon.png',
     iconRetinaUrl: 'assets/marker-icon-2x.png',
     shadowUrl: 'assets/marker-shadow.png'
-  })
+  });
 
-  protected readonly options = {
-    layers: [
-      tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { maxZoom: 18, attribution: '...' })
-    ],
-    zoom: 5,
-    center: latLng(46.879966, -121.726909)
-  };
+  private readonly currentPositionObservable = new Observable<LatLng>((subscriber) => {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        subscriber.next(latLng(position.coords.latitude, position.coords.longitude));
+      },
+      (error) => {
+        subscriber.error(error);
+      }
+    );
+  });
+
+  private readonly clickedAtSignal = signal<LatLng | undefined>(undefined);
+
+  constructor() {
+    effect(() => {
+      const clickPos = this.clickedAt();
+      if (clickPos) {
+        this.userMarkerLayer = [marker(clickPos, { icon: this.icon })];
+      }
+    });
+  }
+
+  onMapReady(map: L.Map) {
+    this.currentPositionObservable.subscribe({
+      next: (pos) => { map.setView(pos, 16); },
+      error: (error) => { console.error('Error getting current position:', error); }
+    });
+
+    map.on('click', (e: L.LeafletMouseEvent) => {
+      this.clickedAtSignal.set(e.latlng);
+    });
+  }
 }
