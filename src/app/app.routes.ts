6a231500426b6282aaa3311a4fc70e3f9209c635
blackfellows <6dfdac64-b799-4068-9972-4a1ea68da044@blackfellows.anonaddy.me>
2025-08-10 20:28:49 +0200

    added guards and new components to routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,12 @@
 import { Routes } from '@angular/router';
 import { Homepage } from './homepage/homepage';
 import { UserProfile } from './user-profile/user-profile';
+import { AgencyManagement } from './agency-management/agency-management';
+import { authGuard } from './_guards/auth-guard';
+import { managerGuard } from './_guards/manager-guard';
+import { Login } from './auth/login/login';
+import { Logout } from './auth/logout/logout';
+import { noAuthGuard } from './_guards/no-auth-guard';
 
 export const routes: Routes = [
     {
@@ -8,8 +14,23 @@ export const routes: Routes = [
         component: Homepage,
     },
     {
+        path: 'login',
+        component: Login,
+        canActivate: [noAuthGuard]
+    },
+    {
+        path: 'logout',
+        component: Logout,
+        canActivate: [authGuard]
+    },
+    {
         path: 'profile',
         component: UserProfile,
-        // canActivate: [AuthGuard]
+        canActivate: [authGuard]
+    },
+    {
+        path: 'agency',
+        component: AgencyManagement,
+        canActivate: [authGuard, managerGuard]
     }
 ];
