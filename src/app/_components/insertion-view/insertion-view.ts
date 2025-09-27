415d5819cd95e68a09b7d86e5e1f7f58411b3a0f
blackfellows <6dfdac64-b799-4068-9972-4a1ea68da044@blackfellows.anonaddy.me>
2025-09-27 19:14:12 +0200

    description uses ngb accordion module

--- src/app/_components/insertion-view/insertion-view.ts
+++ src/app/_components/insertion-view/insertion-view.ts
@@ -2,10 +2,15 @@ import { Component, Input } from '@angular/core';
 import { MatChipsModule } from '@angular/material/chips';
 import { QuillModule } from "ngx-quill";
 import { InsertionResponseDTO } from '../../_types/insertions/InsertionResponseDTO';
+import { NgbAccordionModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-insertion-view',
-  imports: [QuillModule, MatChipsModule],
+  imports: [
+    QuillModule,
+    MatChipsModule,
+    NgbAccordionModule
+  ],
   templateUrl: './insertion-view.html',
   styleUrl: './insertion-view.scss'
 })
