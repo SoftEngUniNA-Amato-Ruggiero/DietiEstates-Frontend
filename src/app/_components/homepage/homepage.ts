bfe5655e411afae7a61b6cff2d060f2980634de3
blackfellows <6dfdac64-b799-4068-9972-4a1ea68da044@blackfellows.anonaddy.me>
2025-10-22 10:54:52 +0200

    agency upload is now a modal

--- src/app/_components/homepage/homepage.ts
+++ src/app/_components/homepage/homepage.ts
@@ -1,4 +1,4 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, effect, inject, signal, TemplateRef, WritableSignal } from '@angular/core';
 import { AuthService } from '../../_services/auth-service';
 import { UserStateService } from '../../_services/user-state-service';
 import { AgencyUpload } from '../agency-upload/agency-upload';
@@ -7,6 +7,8 @@ import { SavedSearches } from "../saved-searches/saved-searches";
 import { InsertionSearchResultDTO } from '../../_types/insertions/InsertionSearchResultDTO';
 import { Page } from '../../_types/page';
 import { SavedSearchService } from '../../_services/saved-search-service';
+import { NgbActiveModal, NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-homepage',
@@ -22,7 +24,32 @@ export class Homepage {
   readonly authService = inject(AuthService);
   readonly userStateService = inject(UserStateService);
   readonly savedSearchService = inject(SavedSearchService);
+  readonly modalService = inject(NgbModal);
+  readonly toastr = inject(ToastrService);
+
+  constructor() {
+    effect(() => {
+      if (this.userStateService.uploadAgencyResponseSignal()) {
+        this.modalService.dismissAll();
+        this.toastr.success('Agency uploaded successfully', 'Success');
+      }
+    });
+  }
+
+  protected closeResult: WritableSignal<string> = signal('');
+
   protected onSavedSearchResults(res: Page<InsertionSearchResultDTO>) {
     this.savedSearchService.savedSearchResults.set(res);
+  }
+
+  protected open(content: TemplateRef<any>) {
+    this.modalService.open(content, { ariaLabelledBy: 'modal-basic-title' }).result.then(
+      (result) => {
+        this.closeResult.set(`Closed with: ${result}`);
+      },
+      (reason) => {
+        // this.closeResult.set(`Dismissed ${this.getDismissReason(reason)}`);
+      },
+    );
   }
 }
